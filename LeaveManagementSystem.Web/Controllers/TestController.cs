9fb1e6beb99aa53605bbb719002198ca4cf4bda8
Joel Hernandez <joel92_hernandez@outlook.com>
2025-01-20 14:56:14 -0700

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel()
+            {
+                Name = "Joel Hernandez",
+                DateOfBirth = new DateTime(1992, 06, 11)
+            };
+            return View(data);
+        }
+    }
+}
